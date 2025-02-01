0b6760dc8545022bd586836b882e309e4ec340fb
NodeWillDev <williamdasilva.dev@gmail.com>
2025-02-01 14:20:20 -0300

    14° commit from 2025

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import { BrowserWindow, app } from "electron";
 
 app.whenReady().then(async () => {
   const window = new BrowserWindow({
-    width: 500,
-    height: 100,
-    titleBarStyle: "hidden",
+    width: 800,
+    height: 500,
+    // titleBarStyle: "hidden",
     backgroundColor: "rgba(0, 0, 0, 0.3)",
     transparent: true,
   });
-  // window.setAlwaysOnTop(true, "dock");
+  window.webContents.openDevTools();
   window.loadFile("./src/index.html");
 });
