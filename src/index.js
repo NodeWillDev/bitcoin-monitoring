6943474fa82fdc4218b6b079496290659f2078b4
NodeWillDev <williamdasilva.dev@gmail.com>
2025-02-05 21:25:46 -0300

    30° commit from 2025

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { join } from "node:path";
-import { BrowserWindow, app } from "electron";
+import { BrowserWindow, app, nativeImage } from "electron";
 
 app.whenReady().then(async () => {
   const window = new BrowserWindow({
@@ -8,11 +8,13 @@ app.whenReady().then(async () => {
     titleBarStyle: "hidden",
     backgroundColor: "rgba(0, 0, 0, 0.3)",
     transparent: true,
+    icon: nativeImage.createFromPath("./src/assets/icon/bitcoin.ico"),
     webPreferences: {
       sandbox: false,
       contextIsolation: true,
     },
   });
-  window.webContents.openDevTools();
+
+  // window.webContents.openDevTools();
   window.loadFile("./src/index.html");
 });
