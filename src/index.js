ed8f0c4917af511a2d5fadef20a86016dd2595fb
NodeWillDev <williamdasilva.dev@gmail.com>
2025-02-10 21:16:02 -0300

    41° commit from 2025

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { BrowserWindow, app, nativeImage } from "electron";
 
 app.whenReady().then(async () => {
   const window = new BrowserWindow({
-    width: 800,
-    height: 500,
-    titleBarStyle: "hidden",
+    width: 430,
+    height: 265,
+    // titleBarStyle: "hidden",
     backgroundColor: "rgba(0, 0, 0, 0.3)",
     transparent: true,
     icon: nativeImage.createFromPath("./src/assets/icon/bitcoin.ico"),
